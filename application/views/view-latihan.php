5100d086d85e733f9cacb57a6dec932b497d051f
Abizar23 <kenr6645@gmail.com>
2022-03-22 20:25:32 +0700

    Add contoh1, contoh2, dan contoh3. Delete file index.php

--- application/views/view-latihan.php
+++ application/views/view-latihan.php
@@ -4,6 +4,7 @@
 </head>
 <body>
 Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+Nama saya Muhamad Abizar Fadillah <br>
  Nilai 1 = <?= $nilai1; ?>
  Nilai 2 = <?= $nilai2; ?>
  ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
