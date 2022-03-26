a76ab037679c43176b74d8a5338f65cd90d0ca65
Abizar23 <kenr6645@gmail.com>
2022-03-26 16:33:02 +0700

    Delete index

--- application/views/view-latihan.php
+++ application/views/view-latihan.php
@@ -5,8 +5,8 @@
 <body>
 Halo Kawan.. Yuk kita belajar web programming..!!!<br>
 Nama saya Muhamad Abizar Fadillah <br>
- Nilai 1 = <?= $nilai1; ?>
- Nilai 2 = <?= $nilai2; ?>
+ Nilai 1 = <?= $nilai1; ?> <br>
+ Nilai 2 = <?= $nilai2; ?> <br>
  ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
 $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
 </body>
